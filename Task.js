14753b98ae55cfdc82c200f50c573350abf6d8d9
Abdelaziz Mezri <abdelazizmezri@Abdelazizs-MacBook-Pro.local>
2023-02-17 23:45:45 +0100

    updates for deploy on esprit server for CN1 JSON workshop

--- Task.js
+++ Task.js
@@ -5,42 +5,55 @@ const router = express.Router();
 
 
 
-const pool =mysql.createPool({
-    connectionLimit:10,
-    host:"localhost",
-    user:"root",
-    database:"esprit"
-
-
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "db",
+    user: "root",
+    password: 'root',
+    database: "db"
 })
 
 
-function getConnection(){
+function getConnection() {
     return pool;
 }
 
 
-router.post("/create/:status/:name",(req,res)=>{
+router.get("/create/:status/:name", (req, res) => {
 
     console.log(req.params);
-pool.query("INSERT INTO `task`(`status`,`name`) VALUES (?,?)",
-[
-    req.params.status,
-    req.params.name],
-    (err,user_rows,fields)=>{
+    pool.query("INSERT INTO `task`(`status`,`name`) VALUES (?,?)",
+        [
+            req.params.status,
+            req.params.name],
+        (err, user_rows, fields) => {
+            if(err)
+                console.log(err)
+            res.status(200);
+            res.json(user_rows)
 
-res.status(200);
-res.json(user_rows)
-
-});
+        });
 })
 
 router.get("/get", (req, res) => {
     pool.query("SELECT * FROM `task` ",
-   (err, rows, fields) => {
-        res.status(200)
-       res.send(rows)
-    })
+        (err, rows, fields) => {
+            if(err)
+                console.log(err)
+            res.status(200)
+            res.json(rows)
+        })
+})
+
+router.get("/clearAll", (req, res) => {
+    pool.query("DELETE FROM `task` ",
+        (err, rows, fields) => {
+            if(err)
+                console.log(err)
+            res.status(200)
+            res.send(rows)
+            
+        })
 })
 
 
--- app.js
+++ app.js
@@ -27,32 +27,32 @@ app.use('/task', taskRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.locals.connection = mysql.createConnection({
-		host     : 'localhost',
-		user     : 'root',
-		password : '',
-		database : 'esprit'
+		host: 'db',
+		user: 'root',
+		password: 'root',
+		database: 'db'
 	});
 	res.locals.connect();
-	
 	next();
 });
+
 module.exports = app;
